b9970c292b81fb5a201cf09c9602c8af93387f7a
SattikBhowmick25 <sattikbhowmick2002@gmail.com>
2025-05-08 02:56:34 +0530

    Initial portfolio

---
+++ src/components/About/About.jsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import ReactTypingEffect from 'react-typing-effect';
+import Tilt from 'react-parallax-tilt';
+import profileImage from '../../assets/pfp3.jpg';
+
+const About = () => {
+  return (
+    <section
+      id="about"
+      className="py-4 px-[7vw] md:px-[7vw] lg:px-[20vw] font-sans mt-0 md:mt-20 lg:mt-20"
+    >
+      <div className="flex flex-col-reverse md:flex-row justify-between items-center">
+        {/* Left Side */}
+        <div className="md:w-1/2 text-center md:text-left mt-8 md:mt-0">
+          {/* Greeting */}
+          <h1 className="text-xl sm:text-2xl md:text-3xl font-bold text-white mb-2 leading-tight">
+            Hi, I am
+          </h1>
+          {/* Name */}
+          <h2 className="text-2xl sm:text-3xl md:text-4xl font-bold text-white mb-4 leading-tight">
+            Sattik Bhowmick
+          </h2>
+          {/* Skills Heading with Typing Effect */}
+          <h3 className="text-lg sm:text-xl md:text-xl font-semibold mb-4 text-[#8245ec] leading-tight">
+            <span className="text-white">I am a </span>
+            <ReactTypingEffect
+              text={[
+                'Full Stack Developer',
+              ]}
+              speed={100}
+              eraseSpeed={50}
+              typingDelay={500}
+              eraseDelay={2000}
+              cursorRenderer={(cursor) => (
+                <span className="text-[#8245ec]">{cursor}</span>
+              )}
+            />
+          </h3>
+          {/* About Me Paragraph */}
+          <p className="text-base sm:text-base md:text-base text-gray-400 mb-10 mt-8 leading-relaxed">
+            I am an MCA(master of Computer Application) student of Jadavpur University. Skilled in both front-end and
+            back-end development, I specialize in the MERN stack and other
+            modern technologies to create seamless user experiences and
+            efficient solutions.
+          </p>
+          {/* Resume Button */}
+          <a
+            href="https://drive.google.com/file/d/1IHc9Qh4uX4Vz-ls2k7N8ukhzrSneBxGF/view?usp=drive_link"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="inline-block text-white py-2 px-6 rounded-full mt-0 text-md font-bold transition duration-300 transform hover:scale-105"
+            style={{
+              background: 'linear-gradient(90deg, #8245ec, #a855f7)',
+              boxShadow: '0 0 2px #8245ec, 0 0 2px #8245ec, 0 0 40px #8245ec',
+            }}
+          >
+            DOWNLOAD RESUME
+          </a>
+          
+        </div>
+        {/* Right Side */}
+        <div className="md:w-1/2 flex justify-center md:justify-end mt-0">
+          <Tilt
+            className="w-24 h-24 sm:w-48 sm:h-48 md:w-[20rem] md:h-[20rem] border-4 border-purple-700 rounded-full"
+            tiltMaxAngleX={20}
+            tiltMaxAngleY={20}
+            perspective={1000}
+            scale={1.05}
+            transitionSpeed={1000}
+            gyroscope={true}
+          >
+            <img
+              src={profileImage}
+              alt="Sattik Bhowmick"
+              className="w-full h-full rounded-full object-cover drop-shadow-[0_10px_20px_rgba(130,69,236,0.5)]"
+            />
+          </Tilt>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
---
+++ src/constants.js
@@ -0,0 +1,113 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import sassLogo from './assets/tech_logo/sass.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import materialuiLogo from './assets/tech_logo/materialui.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+import javaLogo from './assets/tech_logo/java.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+
+
+// Experience Section Logo's
+
+
+// Education Section Logo's
+import juLogo from './assets/education_logo/ju.jpg';
+import jccLogo from './assets/education_logo/jcc.jpeg';
+import jhsLogo from './assets/education_logo/jhs.png';
+
+// Project Section Logo's
+
+
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      { name: 'Bootstrap', logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+      { name: 'Node JS', logo: nodejsLogo },
+      { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'C', logo: cLogo },
+      { name: 'C++', logo: cppLogo },
+      { name: 'Java', logo: javaLogo },
+      { name: 'Python', logo: pythonLogo },
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+    ],
+  },
+];
+
+  
+export const education = [
+    {
+      id: 0,
+      img: juLogo,
+      school: "Jadavpur University, Kolkata",
+      date: "2024 - 2026",
+      grade: "-",
+      desc: "I am pursuing Master's degree (MCA) in Computer Applications from Jadavpur University. I have been gaining a strong foundation in programming, software development, and core computer science subjects. I have studied courses such as Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems etc. My experience at Jadavpur University has been instrumental in shaping my technical abilities and professional growth.",
+      degree: "Master of Computer Applications - MCA",
+    },
+    {
+      id: 1,
+      img: jccLogo,
+      school: "BSA College, Mathura",
+      date: "2021 - 2024",
+      grade: "9.107 CGPA",
+      desc: "I completed my Bachelor's degree in Computer Science (B.Sc.) from Jogesh Chandra Chaudhuri College, Kolkata. Throughout the B.Sc. course, I was immersed in a variety of subjects from exploring Data Structures and Algorithms to diving into Web Development, Machine learning.",
+      degree: "Bachelor of Science - B.Sc Honours in Computer Science",
+    },
+    {
+      id: 2,
+      img: jhsLogo,
+      school: "Jadavpur High School, Kolkata",
+      date: "2019 - 2021",
+      grade: "90.2%",
+      desc: "I completed my higher secondary education from Jadavpur High School, Kolkata, under the WBCHSE board, where I studied Physics, Chemistry, and Mathematics (PCM) with Computer Science.",
+      degree: "WBCHSE(XII) - PCM with Computer Science",
+    },
+    {
+      id: 3,
+      img: jhsLogo,
+      school: "Jadavpur High School, Kolkata",
+      date: "2013 - 2019",
+      grade: "89.71%",
+      desc: "I completed my secondary education from Jadavpur High School, Kolkata, under the WBBSE board",
+      degree: "WBBSE(X) - 10th Grade",
+    },
+];
