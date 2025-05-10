5237cc09fa27f4efe318ffa36e65727bd5c7aa80
SattikBhowmick25 <sattikbhowmick2002@gmail.com>
2025-05-10 18:54:44 +0530

    Section gradient removed

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -16,7 +16,7 @@ const Projects = () => {
   return (
     <section
       id="work"
-      className="py-24 pb-24 px-[12vw] md:px-[7vw] lg:px-[20vw] font-sans relative bg-skills-gradient clip-path-custom"
+      className="py-24 pb-24 px-[12vw] md:px-[7vw] lg:px-[20vw] font-sans relative"
     >
       {/* Section Title */}
       <div className="text-center mb-16">
